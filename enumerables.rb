f5a1270b205c10ce355f6586226b7fbf21d43e8b
priscilla <prolajumokeoni@gmail.com>
2021-05-03 14:28:28 +0100

    fix offenses

--- enumerables.rb
+++ enumerables.rb
@@ -1,12 +1,14 @@
 module Enumerable
   def my_each(&block)
     return to_enum(:my_each) unless block_given?
+
     to_a.each(&block)
     self
   end
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
+
     (0...to_a.length).each do |i|
       yield(to_a[i], i)
     end
@@ -15,6 +17,7 @@ module Enumerable
 
   def my_select
     return to_enum(:my_each) unless block_given?
+
     my_array = []
     my_each do |item|
       my_array.push(item) if yield(i)
@@ -29,7 +32,7 @@ module Enumerable
         result = false unless arg[0] === item
       end
 
-    elsif !block_given? 
+    elsif !block_given?
       result = true
       my_each do |item|
         result = false unless item
@@ -42,26 +45,24 @@ module Enumerable
     end
     result
   end
-  
+
   def my_any?(*arg)
     result = false
     if !arg[0].nil?
       my_each do |item|
-        result = true  if arg[0] === item
+        result = true if arg[0] === item
       end
     elsif !block_given?
       result = false
       my_each do |item|
-        result = true if  item
-    end
+        result = true if item
+      end
     else
       result = false
       my_each do |item|
-        result = true if  yield(item)
+        result = true if yield(item)
       end
     end
     result
   end
 end
-
-p check = [].my_any?  
