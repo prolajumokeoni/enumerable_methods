d07d0af9b77d1186b07cf4cb7fda2ecbe3640882
priscilla <prolajumokeoni@gmail.com>
2021-05-06 11:31:57 +0100

    Update my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -3,17 +3,15 @@ module Enumerable
   def my_each(&block)
     return to_enum(:my_each) unless block_given?
 
-    each(&block)
+    to_a.each(&block)
     self
   end
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-    i = 0
-    while i < length
-      yield i, self[i]
-      i += 1
+    (0...to_a.length).each do |i|
+      yield(to_a[i], i)
     end
     self
   end
