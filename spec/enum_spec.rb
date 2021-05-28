4ecd71f0ec2892ee16bf574e89e25087bceea996
priscilla <you@example.com>
2021-05-28 14:50:55 +0100

    Add test for my_each, my_each_with_index and my_select

---
+++ spec/enum_spec.rb
@@ -0,0 +1,48 @@
+require_relative  '../enumerables.rb'
+
+
+describe Enumerable do 
+  let(:integer_arr) { [1, 2, 3, 4, 5] }
+  let(:string_arr) { %w[apple peach mango]}
+  let(:combined_arr) { [5, true, nil] }
+  let(:empty_arr) { [] }
+
+  describe '#my_each' do
+    it 'iterates over every element in the array' do
+      expect(integer_arr.my_each { |i| i * 2} ).to eql([1, 2, 3, 4, 5])
+    end
+    it 'iterates over every element in the array' do
+      expect(string_arr.my_each {|i| "#{i}"} ).to eql(%w[apple peach mango])
+    end
+    it 'returns an enumerator if no block is given' do
+      expect(integer_arr.my_each).to be_a(Enumerator)
+    end
+  end
+
+
+  describe '#my_each_with_index' do
+    it 'iterates over every element with its index' do
+      expect(integer_arr.my_each_with_index { |item, index| item if index.even?}).to eql([1, 2, 3, 4, 5])
+    end
+    it 'iterates over every element with its index' do
+      expect(integer_arr.my_each_with_index { |item, index| "#{item}"}).to eql([1, 2, 3, 4, 5])
+    end
+    it 'returns an enumerator if no block is given' do
+      expect(integer_arr.my_each).to be_a(Enumerator)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns new array that pass requirements' do
+      expect(integer_arr.my_select(&:even?)).to eql([2, 4])
+    end
+    it 'returns new array that pass requirements' do
+      expect(string_arr.my_select { |item| item.length > 1}).to eql( %w[apple peach mango]) 
+    end
+    it 'returns an enumerator if no block is given' do
+      expect(integer_arr.my_select).to be_a(Enumerator)
+    end
+  end
+
+
+end
